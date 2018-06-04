482d13d4cc272f021993e31376e2b50597a9c594
Mark Tice <marktice@protonmail.com>
2018-06-04 19:37:19 +1000

    Add projects page

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.use((req, res, next) => {
   });
   next();
 });
+
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -41,6 +43,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    projectsMessage: 'Portfolio page here',
   });
 });
 
